1d34c6861e868f3f457fb0784f2a84e5bae2ffbc
Sean Perkins <sean@meetmaestro.com>
2016-09-07 07:41:16 -0400

    Demo update, fixes typing definitions, version bump

--- demo/app/main-page.ts
+++ demo/app/main-page.ts
@@ -3,32 +3,10 @@ import {Page} from 'ui/page';
 import {isAndroid, isIOS} from 'platform';
 
 import {Demo} from './main-view-model';
-import {TNSOTPublisher, TNSOTSubscriber, TNSOTSession} from 'nativescript-opentok';
 
 export function pageLoaded(args: EventData) {
     var page = <Page>args.object;
     page.bindingContext = new Demo(page);
 }
-
-export function publisherLoaded(args: EventData) {
-    let publisher = <TNSOTPublisher> args.object;
-    if(isAndroid) {
-        console.log('OpenTok Publisher Native Android: ' + publisher.android);
-    }
-    else if(isIOS) {
-        console.log('OpenTok Publisher Native iOS: ' + publisher.ios);
-    }
-}
-
-export function subscriberLoaded(args: EventData) {
-    let subscriber = <TNSOTSubscriber> args.object;
-    if(isAndroid) {
-        console.log('OpenTok SubScriber Native Android: ' + subscriber.android);
-    }
-    else if(isIOS) {
-        console.log('Opentok Subscriber Native iOS: ' + subscriber.ios);
-    }
-}
-
 
 exports.pageLoaded = pageLoaded;
